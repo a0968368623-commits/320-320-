989d9e3eebbe5fae836ba32b1f63ec23bc070c27
320jjiikk8 <a0968368623@gmail.com>
2026-05-03 12:28:25 +0800

    Update BaseURL to include markdown link format

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- [[ 320 Master 雲端主控 ]]
-local BaseURL = "https://raw.githubusercontent.com/a0968368623-commits/320-320-/main/Modules/"
+local BaseURL = "[https://raw.githubusercontent.com/a0968368623-commits/320-320-/main/Modules/](https://raw.githubusercontent.com/a0968368623-commits/320-320-/main/Modules/)"
 
 local function GetCloud(File)
     local s, content = pcall(game.HttpGet, game, BaseURL .. File .. "?t=" .. os.time())
