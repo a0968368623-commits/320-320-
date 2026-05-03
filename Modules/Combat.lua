2722805366d92e94c09a529b1f7b858fac00be72
320jjiikk8 <a0968368623@gmail.com>
2026-05-03 11:02:46 +0800

    Replace Combat_Mod with Combat for hitbox control
    Refactor combat module to improve hitbox management.

--- Modules/Combat.lua
+++ Modules/Combat.lua
@@ -1,14 +1,12 @@
--- [[ V1 320 戰鬥模塊 ]]
-local Combat_Mod = {}
-function Combat_Mod:Init(Settings)
-    -- 這裡專門放 Hitbox 巨大化與自動攻擊邏輯
-    -- 修復你影片中 Hitbox 消失的 Bug
-    task.spawn(function()
-        while task.wait(0.5) do
-            if Settings.Hitbox then
-                -- 執行 Hitbox 注入...
-            end
-        end
-    end)
+local Combat = {}
+
+function Combat:SetHitbox(State, Size)
+    if State then
+        print("[戰鬥] Hitbox 已開啟，大小：" .. Size)
+        -- 這裡寫入原本的循環 Hitbox 邏輯
+    else
+        print("[戰鬥] Hitbox 已關閉")
+    end
 end
-return Combat_Mod
+
+return Combat
